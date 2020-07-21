0942bdf5a483bb4c5a9cc2d63c496fe9d3079762
tomito26 <tommybwah@gmail.com>
2020-07-22 00:56:49 +0300

    added a new project which involves the changing of the background

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('button#green').click(function() {
+        $('body').removeClass()
+        $('body').addClass('green-background');
+    });
+
+    $('button#yellow').click(function() {
+        $('body').removeClass()
+        $('body').addClass('yellow-background');
+    });
+
+    $('button#red').click(function() {
+        $('body').removeClass()
+        $('body').addClass('red-background');
+    });
+    $('button#grey').click(function(){
+        $('body').removeClass
+        $('body').addClass('grey-background');
+    });
+    $('button#blue').click(function() {
+        $('body').removeClass()
+        $('body').addClass('blue-background');
+    });
+});
